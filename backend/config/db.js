e8c3f93b864a4e8bd6e55b74934b65b76066027a
akhil-220923 <akhil.220923@rguktn.ac.in>
2026-05-16 11:25:20 +0530

    fix mongodb env variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/minibank";
+    const uri = process.env.MONGO_URI;
     await mongoose.connect(uri);
 
     console.log("MongoDB Connected");
