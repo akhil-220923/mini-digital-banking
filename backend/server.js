f6a5be59862ffed987d894eb8dbdc31e7afa71fd
akhil-220923 <akhil.220923@rguktn.ac.in>
2026-05-16 11:44:00 +0530

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
 app.use(errorHandler);
 
 // Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
