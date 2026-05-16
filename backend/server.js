d790fa5d54b9fc59ee075040912e6af9008f2d05
akhil-220923 <akhil.220923@rguktn.ac.in>
2026-05-16 13:49:46 +0530

    update cors with latest netlify url

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors({
     origin: [
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "https://superb-chebakia-3e4dce.netlify.app"
+        "https://guileless-sunburst-b156f0.netlify.app"
     ],
     credentials: true
 }));
--- public/js/auth.js
+++ public/js/auth.js
@@ -5,7 +5,7 @@
 
 const API_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
   ? "http://localhost:5000/api"
-  : "https://mini-banking-backend.onrender.com/api"; // ⚠️ Replace with your Render URL after deployment
+  : https://mini-banking-backend.onrender.com/api"; // ⚠️ Replace with your Render URL after deployment
 const SESSION_KEY = "nexusSession";
 
 /* ===============================
