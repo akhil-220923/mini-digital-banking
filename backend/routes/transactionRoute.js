dc37070e1f3b1cf29ed8ba242b28d8d9bee1a327
akhil-220923 <akhil.220923@rguktn.ac.in>
2026-05-16 11:10:27 +0530

    fix transaction import

--- backend/routes/transactionRoute.js
+++ backend/routes/transactionRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Transaction = require("../models/transaction");
+const Transaction = require("../models/Transaction");
 const { verifyUser } = require("../middleware/auth");
 
 /**
