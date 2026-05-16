df4b662f018ad5e018c93f92d9f15b821d8dbdb7
akhil-220923 <akhil.220923@rguktn.ac.in>
2026-05-16 13:00:47 +0530

    add netlify frontend url

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,14 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://superb-chebakia-3e4dce.netlify.app"
+    ],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
